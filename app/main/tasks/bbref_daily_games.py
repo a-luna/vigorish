6724754b601f57ca79721665e8f055f42d531e53
Aaron Luna <aaron@alunablog.com>
2019-06-17 13:11:46 -0700

    minor import change

--- app/main/tasks/bbref_daily_games.py
+++ app/main/tasks/bbref_daily_games.py
@@ -2,7 +2,6 @@ from app.main.scrape.bbref.scrape_bbref_games_for_date import scrape_bbref_games
 from app.main.status.update_status_bbref_games_for_date import update_bbref_games_for_date_single_date
 from app.main.tasks.base_task import BaseTask
 from app.main.util.s3_helper import upload_bbref_games_for_date
-from app.main.util.scrape_functions import get_chromedriver
 
 class ScrapeBBRefDailyGames(BaseTask):
     key_name = "bbref_games_for_date"
