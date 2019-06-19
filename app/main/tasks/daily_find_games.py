21c3ab1c261a94791d18e325aba9abaad95a18e2
Aaron Luna <aaron@alunablog.com>
2019-06-19 04:00:28 -0700

    debugging...

--- app/main/tasks/daily_find_games.py
+++ app/main/tasks/daily_find_games.py
@@ -11,24 +11,39 @@ class FindAllGamesForDateTask(BaseTask):
     display_name = "Games for date (bbref.com)"
 
     def execute(self, scrape_date):
+        print("STARTED: Scrape BBref")
         result = scrape_bbref_games_for_date(scrape_date)
         if result.failure:
             return result
         bbref_games_for_date = result.value
+        print("FINISHED: Scrape BBref")
+        print("STARTED: Upload BBref")
         result = upload_bbref_games_for_date(bbref_games_for_date)
         if result.failure:
             return result
+        print("FINISHED: Upload BBref")
+        print("STARTED: Update BBref")
         result =  update_bbref_games_for_date_single_date(
             self.db['session'], self.season, bbref_games_for_date)
         if result.failure:
             return result
+        print("FINISHED: Update BBref")
+        print("STARTED: Scrape Brooks")
         result = scrape_brooks_games_for_date(
             self.db['session'], scrape_date, bbref_games_for_date)
         if result.failure:
             return result
         brooks_games_for_date = result.value
+        print("FINISHED: Scrape Brooks")
+        print("STARTED: Upload Brooks")
         result = upload_brooks_games_for_date(brooks_games_for_date)
         if result.failure:
             return result
-        return update_brooks_games_for_date_single_date(
+        print("FINISHED: Upload Brooks")
+        print("STARTED: Update Brooks")
+        result = update_brooks_games_for_date_single_date(
             self.db['session'], self.season, brooks_games_for_date, scrape_date)
+        if result.failure:
+            return result
+        print("FINISHED: Update Brooks")
+        return Result.Ok()
