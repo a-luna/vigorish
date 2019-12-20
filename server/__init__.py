177a519ad7e355af21ac48dac323895e0a76cebc
Aaron Luna <aaron@alunablog.com>
2019-12-19 20:57:46 -0800

    config

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 """Flask app initialization via factory pattern."""
+import json
 import os
 from pathlib import Path
 
@@ -8,15 +9,16 @@ from sqlalchemy.orm import sessionmaker
 
 
 APP_FOLDER = Path(__file__).resolve().parent.parent
+CONFIG_PATH = APP_FOLDER / "config.json"
 
 def create_app():
     app = Flask("vigorish-app", root_path=APP_FOLDER)
+    config = json.loads(CONFIG_PATH.read_text())
+    engine = create_engine(config.get("DATABASE_URL"))
+    session_maker = sessionmaker(bind=engine)
+    app.session = session_maker()
 
     from server.frontend import frontend
     app.register_blueprint(frontend)
-
-    engine = create_engine(os.getenv("DATABASE_URL"))
-    session_maker = sessionmaker(bind=engine)
-    app.session = session_maker()
 
     return app
