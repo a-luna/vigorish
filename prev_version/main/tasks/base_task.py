0989320e6b7933785ca0ec4d1bd978859a4aacee
Aaron Luna <admin@aaronluna.dev>
2020-03-27 17:58:01 -0700

    removed previous project folders/files

--- prev_version/main/tasks/base_task.py
+++
@@ -1,8 +0,0 @@
-class BaseTask:
-    key_name = ""
-    display_name = ""
-
-    def __init__(self, db, season, driver):
-        self.db = db
-        self.season = season
-        self.driver = driver
