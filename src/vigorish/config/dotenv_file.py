777003117ca2174d0cc4774a3446e790bfc3d51e
Aaron Luna <admin@aaronluna.dev>
2020-05-20 15:33:08 -0700

    for testing, add ability to specify location of .env file and add function to create default .env file

--- src/vigorish/config/dotenv_file.py
+++ src/vigorish/config/dotenv_file.py
@@ -11,27 +11,33 @@ DOTENV_FILE = VIG_FOLDER / ".env"
 DEFAULT_CONFIG = VIG_FOLDER / "vig.config.json"
 
 
+def create_default_dotenv_file(dotenv_file, config_file=None, db_url=None):
+    env_var_dict = {var_name: "" for var_name in ENV_VAR_NAMES}
+    env_var_dict["CONFIG_FILE"] = config_file if config_file else DEFAULT_CONFIG
+    env_var_dict["DATABASE_URL"] = db_url if db_url else SQLITE_PROD_URL
+    env_var_strings = [f"{name}={value}" for name, value in env_var_dict.items()]
+    dotenv_file.write_text("\n".join(env_var_strings))
+
+
 class DotEnvFile:
-    def __init__(self):
+    def __init__(self, dotenv_filepath=None):
+        if dotenv_filepath:
+            self.dotenv_filepath = dotenv_filepath
+        else:
+            self.dotenv_filepath = DOTENV_FILE
         self.env_var_dict = self.read_dotenv_file()
 
     def read_dotenv_file(self):
         """Parse .env file to a dictionary of environment variables."""
-        if not DOTENV_FILE.exists():
-            self.create_default_dotenv_file()
-        if not DOTENV_FILE.is_file():
-            raise TypeError(f"Unable to open file: {DOTENV_FILE}")
-        load_dotenv(DOTENV_FILE)
-        file_text = DOTENV_FILE.read_text()
+        if not self.dotenv_filepath.exists():
+            create_default_dotenv_file(dotenv_file=self.dotenv_filepath)
+        if not self.dotenv_filepath.is_file():
+            raise TypeError(f"Unable to open file: {self.dotenv_filepath}")
+        load_dotenv(self.dotenv_filepath)
+        file_text = self.dotenv_filepath.read_text()
         env_var_split = [f for f in file_text.split("\n") if f and "=" in f]
         env_var_split = [s.split("=") for s in env_var_split]
         return {v[0]: v[1].strip('"').strip("'").strip() for v in env_var_split if len(v) == 2}
-
-    def create_default_dotenv_file(self):
-        self.env_var_dict = {var_name: "" for var_name in ENV_VAR_NAMES}
-        self.change_value("CONFIG_FILE", DEFAULT_CONFIG)
-        self.change_value("DATABASE_URL", SQLITE_PROD_URL)
-        self.write_dotenv_file()
 
     def get_current_value(self, env_var_name):
         if env_var_name not in ENV_VAR_NAMES:
@@ -51,4 +57,4 @@ class DotEnvFile:
 
     def write_dotenv_file(self):
         env_var_strings = [f"{name}={value}" for name, value in self.env_var_dict.items()]
-        DOTENV_FILE.write_text("\n".join(env_var_strings))
+        self.dotenv_filepath.write_text("\n".join(env_var_strings))
