23e2bdf1aae9d4674edb463b4821af84798fb397
Aaron Luna <admin@aaronluna.dev>
2020-06-25 04:06:54 -0700

    complete refactoring of node scripts

--- src/vigorish/nightmarejs/scrape_job.js
+++ src/vigorish/nightmarejs/scrape_job.js
@@ -2,88 +2,89 @@ const Xvfb = require("xvfb")
 const parseArgs = require("minimist")
 const Nightmare = require("./new_nightmare")
 const { executeBatchJob, scrapeUrls } = require("./scrape_urls")
+require("events").EventEmitter.defaultMaxListeners = 0
 
 const args = parseArgs(process.argv.slice(2))
 main(args).catch(console.error)
 
 async function main(args) {
-  const urlSetFilepath = args.urlSetFilepath
-  const timeoutParams = getTimeoutParams(args)
-  const batchJobParams = getBatchJobParams(args)
-  const nightmare = Nightmare({
-    show: false,
-    pollInterval: 50,
-    waitTimeout: 150000,
-    gotoTimeout: 150000,
-  })
-  const xvfb = new Xvfb()
-  xvfb.startSync()
+    const urlSetFilepath = args.urlSetFilepath
+    const timeoutParams = getTimeoutParams(args)
+    const batchJobParams = getBatchJobParams(args)
+    const nightmare = Nightmare({
+        show: false,
+        pollInterval: 50,
+        waitTimeout: 150000,
+        gotoTimeout: 150000,
+    })
+    const xvfb = new Xvfb()
+    xvfb.startSync()
 
-  const [success, err] = await poss(
-    run(nightmare, urlSetFilepath, timeoutParams, batchJobParams)
-  )
-  xvfb.stopSync()
-  if (!success) {
-    throw err
-  }
-  process.exit(0)
+    const [success, err] = await poss(
+        run(nightmare, urlSetFilepath, timeoutParams, batchJobParams)
+    )
+    xvfb.stopSync()
+    if (!success) {
+        throw err
+    }
+    process.exit(0)
 }
 
 function getTimeoutParams(args) {
-  let timeoutParams = { urlTimeoutRequired: false, batchTimeoutRequired: false }
-  if ("urlTimeoutRequired" in args) {
-    timeoutParams.urlTimeoutRequired = true
-    if ("urlTimeoutMinMs" in args && "urlTimeoutMaxMs" in args) {
-      timeoutParams.urlTimeoutMinMs = args.urlTimeoutMinMs
-      timeoutParams.urlTimeoutMaxMs = args.urlTimeoutMaxMs
-    } else if ("urlTimeoutUniformMs" in args) {
-      timeoutParams.urlTimeoutMinMs = args.urlTimeoutUniformMs
-      timeoutParams.urlTimeoutMaxMs = args.urlTimeoutUniformMs
+    let timeoutParams = { urlTimeoutRequired: false, batchTimeoutRequired: false }
+    if ("urlTimeoutRequired" in args) {
+        timeoutParams.urlTimeoutRequired = true
+        if ("urlTimeoutMinMs" in args && "urlTimeoutMaxMs" in args) {
+            timeoutParams.urlTimeoutMinMs = args.urlTimeoutMinMs
+            timeoutParams.urlTimeoutMaxMs = args.urlTimeoutMaxMs
+        } else if ("urlTimeoutUniformMs" in args) {
+            timeoutParams.urlTimeoutMinMs = args.urlTimeoutUniformMs
+            timeoutParams.urlTimeoutMaxMs = args.urlTimeoutUniformMs
+        }
     }
-  }
-  if ("batchTimeoutRequired" in args) {
-    timeoutParams.batchTimeoutRequired = true
-    if ("batchTimeoutMinMs" in args && "batchTimeoutMaxMs" in args) {
-      timeoutParams.batchTimeoutMinMs = args.batchTimeoutMinMs
-      timeoutParams.batchTimeoutMaxMs = args.batchTimeoutMaxMs
-    } else if ("batchTimeoutUniformMs" in args) {
-      timeoutParams.batchTimeoutMinMs = args.batchTimeoutUniformMs
-      timeoutParams.batchTimeoutMaxMs = args.batchTimeoutUniformMs
+    if ("batchTimeoutRequired" in args) {
+        timeoutParams.batchTimeoutRequired = true
+        if ("batchTimeoutMinMs" in args && "batchTimeoutMaxMs" in args) {
+            timeoutParams.batchTimeoutMinMs = args.batchTimeoutMinMs
+            timeoutParams.batchTimeoutMaxMs = args.batchTimeoutMaxMs
+        } else if ("batchTimeoutUniformMs" in args) {
+            timeoutParams.batchTimeoutMinMs = args.batchTimeoutUniformMs
+            timeoutParams.batchTimeoutMaxMs = args.batchTimeoutUniformMs
+        }
     }
-  }
-  return timeoutParams
+    return timeoutParams
 }
 
 function getBatchJobParams(args) {
-  let batchJobParams = { batchScrapingEnabled: false }
-  if ("batchScrapingEnabled" in args) {
-    batchJobParams.batchScrapingEnabled = true
-    if ("minBatchSize" in args && "maxBatchSize" in args) {
-      batchJobParams.batchSizeIsRandom = true
-      batchJobParams.minBatchSize = args.minBatchSize
-      batchJobParams.maxBatchSize = args.maxBatchSize
-    } else if ("uniformBatchSize" in args) {
-      batchJobParams.batchSizeIsRandom = false
-      batchJobParams.uniformBatchSize = args.uniformBatchSize
+    let batchJobParams = { batchScrapingEnabled: false }
+    if ("batchScrapingEnabled" in args) {
+        batchJobParams.batchScrapingEnabled = true
+        if ("minBatchSize" in args && "maxBatchSize" in args) {
+            batchJobParams.batchSizeIsRandom = true
+            batchJobParams.minBatchSize = args.minBatchSize
+            batchJobParams.maxBatchSize = args.maxBatchSize
+        } else if ("uniformBatchSize" in args) {
+            batchJobParams.batchSizeIsRandom = false
+            batchJobParams.uniformBatchSize = args.uniformBatchSize
+        }
     }
-  }
-  return batchJobParams
+    return batchJobParams
 }
 
 async function run(nightmare, urlSetFilepath, timeoutParams, batchJobParams) {
-  if (batchJobParams.batchScrapingEnabled) {
-    await executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeoutParams)
-  } else {
-    await scrapeUrls(nightmare, urlSetFilepath, timeoutParams)
-  }
-  await nightmare.end()
+    if (batchJobParams.batchScrapingEnabled) {
+        await executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeoutParams)
+    } else {
+        await scrapeUrls(nightmare, urlSetFilepath, timeoutParams)
+    }
+    await nightmare.end()
 }
 
 async function poss(promise) {
-  try {
-    await promise
-    return [true, null]
-  } catch (err) {
-    return [false, err]
-  }
+    try {
+        await promise
+        return [true, null]
+    } catch (err) {
+        return [false, err]
+    }
 }
--- src/vigorish/nightmarejs/scrape_urls.js
+++ src/vigorish/nightmarejs/scrape_urls.js
@@ -4,8 +4,6 @@ const colors = require("colors")
 var joinPath = require("path.join")
 const { makeChunkedList, makeIrregularChunkedList } = require("./list_functions")
 
-const PBAR_MAX_LENGTH = 23
-
 const multibar = new cliProgress.MultiBar({
     format: "{message} " + colors.cyan("|{bar}| ") + " {percentage}% | {value}/{total} {unit}",
     barCompleteChar: "\u2588",
@@ -17,41 +15,46 @@ const multibar = new cliProgress.MultiBar({
     autopadding: true,
 })
 
+let PBAR_MAX_LENGTH
+const PBAR_MAX_LEN_SINGLE_BATCH = 20
+const PBAR_MAX_LEN_LT_100_URLS = 20
+const PBAR_MAX_LEN_LT_1K_URLS = 22
+const PBAR_MAX_LEN_LT_10K_URLS = 24
+const PBAR_MAX_LEN_GT_10K_URLS = 26
+
 async function executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeoutParams) {
     let batchCounter = 0
     let urlCounter = 0
     const allUrls = readUrlSetFromFile(urlSetFilepath)
-    let [batchList, totalBatches, totalUrls] = createBatchJob(allUrls, batchJobParams)
-    let [batchBar, urlBar, timeoutBar] = initializeProgressBars(batchList, totalUrls)
-    await batchList.reduce(async (promise, urlBatch) => {
-        await promise
+    let batchList = createBatchJob(allUrls, batchJobParams)
+    let [batchBar, urlBar, timeoutBar] = initializeProgressBars(batchList, allUrls.length)
+    for await (const urlBatch of batchList) {
         batchBar.update(batchCounter, { message: urlBatchMessage(batchList[batchCounter].length) })
         urlCounter = await scrapeUrlBatch(nightmare, urlBatch, timeoutParams, urlCounter, urlBar)
         batchCounter += 1
-        if (batchCounter < totalBatches) {
+        if (batchCounter < batchList.length) {
             const nextBatchSize = batchList[batchCounter].length
             batchBar.update(batchCounter, { message: urlBatchMessage(nextBatchSize, true) })
             if (timeoutParams.batchTimeoutRequired) {
                 await batchTimeout(timeoutParams, timeoutBar, urlCounter, nextBatchSize)
             }
         }
-    }, Promise.resolve())
+    }
     batchBar.update(batchCounter, { message: pBarMessage("Scraped all batches") })
     multibar.stop()
     return
 
-    function createBatchJob(allUrls, batchJobParams) {
-        let batchList
-        if (batchJobParams.batchSizeIsRandom) {
-            let { minBatchSize, maxBatchSize } = batchJobParams
-            batchList = makeIrregularChunkedList(allUrls, minBatchSize, maxBatchSize)
-        } else {
-            batchList = makeChunkedList(allUrls, batchJobParams.uniformBatchSize)
-        }
-        return [batchList, batchList.length, allUrls.length]
+    function createBatchJob(
+        allUrls,
+        { batchSizeIsRandom, minBatchSize, maxBatchSize, uniformBatchSize }
+    ) {
+        return batchSizeIsRandom
+            ? makeIrregularChunkedList(allUrls, minBatchSize, maxBatchSize)
+            : makeChunkedList(allUrls, uniformBatchSize)
     }
 
     function initializeProgressBars(batchList, totalUrls) {
+        set_pbar_max_length(batchList.length, totalUrls)
         const timeoutBar = multibar.create(0, 0, {
             message: urlRangeMessage(0, batchList[0].length),
             unit: "",
@@ -67,44 +70,49 @@ async function executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeou
         return [batchBar, urlBar, timeoutBar]
     }
 
+    function set_pbar_max_length(totalBatches, totalUrls) {
+        PBAR_MAX_LENGTH =
+            totalBatches == 1
+                ? PBAR_MAX_LEN_SINGLE_BATCH
+                : totalUrls < 100
+                ? PBAR_MAX_LEN_LT_100_URLS
+                : totalUrls < 1000
+                ? PBAR_MAX_LEN_LT_1K_URLS
+                : totalUrls < 10000
+                ? PBAR_MAX_LEN_LT_10K_URLS
+                : PBAR_MAX_LEN_GT_10K_URLS
+    }
+
     function urlRangeMessage(urlCounter, nextBatchSize) {
-        let nextUrlRange = `Scraping URLs ${urlCounter + 1}-${urlCounter + nextBatchSize}`
-        return pBarMessage(nextUrlRange)
+        return pBarMessage(`Scraping URLs ${urlCounter + 1}-${urlCounter + nextBatchSize}`)
     }
 
     function urlBatchMessage(batchSize, isTimeout = false) {
-        const thisNext = isTimeout ? "next" : "this"
-        const batchMessage = `${batchSize} URLs ${thisNext} batch`
-        return pBarMessage(batchMessage)
+        return pBarMessage(`${batchSize} URLs ${isTimeout ? "next" : "this"} batch`)
     }
 
     function pBarMessage(input) {
-        let padLength = PBAR_MAX_LENGTH - input.length
-        if (padLength <= 0) {
-            return input
-        }
-        return `${" ".repeat(padLength)}${input}`
+        return input.length >= PBAR_MAX_LENGTH
+            ? input
+            : `${" ".repeat(PBAR_MAX_LENGTH - input.length)}${input}`
     }
 
     async function scrapeUrlBatch(nightmare, urlSet, timeoutParams, urlCounter, progressBar) {
-        await urlSet.reduce(async (promise, urlDetails) => {
-            await promise
-            let { url, fileName, identifier, scrapedHtmlFolderpath } = urlDetails
+        for await (const { url, fileName, identifier, scrapedHtmlFolderpath } of urlSet) {
             progressBar.update(urlCounter, { message: pBarMessage(identifier) })
             await scrapeUrl(nightmare, url, fileName, scrapedHtmlFolderpath, timeoutParams)
             urlCounter += 1
-        }, Promise.resolve())
+        }
         progressBar.update(urlCounter)
         return urlCounter
     }
 
     async function batchTimeout(timeoutParams, progressBar, urlCounter, nextBatchSize) {
         let [timeoutList, minutes, seconds] = getRandomTimeout(timeoutParams)
-        progressBar.setTotal(timeoutList.length)
-        progressBar.update(0, { message: timeoutMessage(minutes, seconds), unit: "Seconds" })
-        let counter = 0
-        await timeoutList.reduce(async (promise, timeout) => {
-            await promise
+        let secondsRemaining = timeoutList.length
+        progressBar.setTotal(secondsRemaining)
+        progressBar.update(secondsRemaining, { message: timeoutMessage(minutes, seconds), unit: "Seconds" })
+        for await (const timeout of timeoutList) {
             await sleep(timeout)
             if (seconds == 0) {
                 minutes -= 1
@@ -112,9 +120,9 @@ async function executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeou
             } else {
                 seconds -= 1
             }
-            counter += 1
-            progressBar.update(counter, { message: timeoutMessage(minutes, seconds) })
-        }, Promise.resolve())
+            secondsRemaining -= 1
+            progressBar.update(secondsRemaining, { message: timeoutMessage(minutes, seconds) })
+        }
         progressBar.setTotal(0)
         progressBar.update(0, { message: urlRangeMessage(urlCounter, nextBatchSize), unit: "" })
         return
@@ -124,14 +132,14 @@ async function executeBatchJob(nightmare, urlSetFilepath, batchJobParams, timeou
             const minutes = Math.floor(timeoutMs / 60000)
             const seconds = Math.floor((timeoutMs - minutes * 60000) / 1000)
             const totalSeconds = Math.floor(timeoutMs / 1000)
-            const timeoutList = Array.from({ length: totalSeconds }).map((x) => 1000)
+            const timeoutList = Array(totalSeconds).fill(1000)
             return [timeoutList, minutes, seconds]
         }
 
         function timeoutMessage(minutes, seconds) {
-            const minPadded = minutes.toString().padStart(2, "0")
-            const secPadded = seconds.toString().padStart(2, "0")
-            return pBarMessage(`${minPadded}:${secPadded} until next batch`)
+            const minPad = minutes.toString().padStart(2, "0")
+            const secPad = seconds.toString().padStart(2, "0")
+            return pBarMessage(`${minPad}:${secPad} until next batch`)
         }
     }
 }
@@ -140,13 +148,11 @@ async function scrapeUrls(nightmare, urlSetFilepath, timeoutParams) {
     const urlSet = readUrlSetFromFile(urlSetFilepath)
     const urlBar = multibar.create(urlSet.length, 0, { message: "N/A", unit: "URLs" })
     let counter = 0
-    await urlSet.reduce(async (promise, urlDetails) => {
-        await promise
-        let { url, fileName, identifier, scrapedHtmlFolderpath } = urlDetails
+    for await (const { url, fileName, identifier, scrapedHtmlFolderpath } of urlSet) {
         urlBar.update(counter, { message: identifier })
         await scrapeUrl(nightmare, url, fileName, scrapedHtmlFolderpath, timeoutParams)
         counter += 1
-    }, Promise.resolve())
+    }
     urlBar.stop()
     return
 }
@@ -157,12 +163,9 @@ async function scrapeUrl(nightmare, url, outputFileName, outputFolderPath, timeo
     writeFileSync(filePath, html)
     return filePath
 
-    async function fetchUrl(url, timeoutParams) {
-        let { urlTimeoutMinMs, urlTimeoutMaxMs } = timeoutParams
+    async function fetchUrl(url, { urlTimeoutMinMs, urlTimeoutMaxMs }) {
         try {
-            await nightmare.on("did-get-response-details", () => {
-                startTime = Date.now()
-            })
+            await nightmare.on("did-get-response-details", () => (startTime = Date.now()))
             await nightmare.goto(url)
             await nightmare.waitUntilNetworkIdle(500)
             let html = await nightmare.evaluate(() => document.body.innerHTML)
@@ -175,8 +178,7 @@ async function scrapeUrl(nightmare, url, outputFileName, outputFolderPath, timeo
 }
 
 function readUrlSetFromFile(urlSetFilepath) {
-    const urlSetText = readFileSync(urlSetFilepath, { encoding: "utf8" })
-    return JSON.parse(urlSetText)
+    return JSON.parse(readFileSync(urlSetFilepath, { encoding: "utf8" }))
 }
 
 function sleep(timeoutMs) {
@@ -184,9 +186,7 @@ function sleep(timeoutMs) {
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min
+    return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + Math.ceil(min)
 }
 
 module.exports = {
