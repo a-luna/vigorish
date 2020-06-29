1041f8646d1bc3427edb96e8fdb235c26dea1fa0
Aaron Luna <admin@aaronluna.dev>
2020-06-29 05:45:12 -0700

    update pypi packages, bump version to 0.2.21

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setup(
     description=DESCRIPTION,
     long_description=README,
     long_description_content_type="text/markdown",
-    version="0.2.20",
+    version="0.2.21",
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     maintainer=AUTHOR,
