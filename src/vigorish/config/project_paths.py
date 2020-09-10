e53035a5a082880d4e6182582b18e7ae12de4c98
Aaron Luna <admin@aaronluna.dev>
2020-09-10 05:59:38 -0700

    add project_paths module
    -prevent duplicating the same path more than once in various places

---
+++ src/vigorish/config/project_paths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+
+APP_FOLDER = Path(__file__).parent.parent
+VIG_FOLDER = Path.home().joinpath(".vig")
+
+NIGHTMAREJS_FOLDER = APP_FOLDER.joinpath("nightmarejs")
+NODEJS_INBOX = NIGHTMAREJS_FOLDER.joinpath("inbox")
+NODEJS_OUTBOX = NIGHTMAREJS_FOLDER.joinpath("outbox")
+NODEJS_SCRIPT = NIGHTMAREJS_FOLDER.joinpath("scrape_job.js")
+
+CSV_FOLDER = APP_FOLDER.joinpath("setup/csv")
+PEOPLE_CSV = CSV_FOLDER.joinpath("People.csv")
+TEAM_CSV = CSV_FOLDER.joinpath("Team.csv")
+PLAYER_ID_MAP_CSV = CSV_FOLDER.joinpath("bbref_player_id_map.csv")
+PLAYER_TEAM_MAP_CSV = CSV_FOLDER.joinpath("bbref_player_team_map.csv")
