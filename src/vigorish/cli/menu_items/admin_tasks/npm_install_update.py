41a6d489484b589888577fe0bbf668a5bbf07af7
Aaron Luna <admin@aaronluna.dev>
2020-09-10 01:39:07 -0700

    add Task: Install/Update NPM Packages
    - refactored menu item for updating npm packages as a Task instance

--- src/vigorish/cli/menu_items/admin_tasks/npm_install_update.py
+++ src/vigorish/cli/menu_items/admin_tasks/npm_install_update.py
@@ -1,6 +1,5 @@
 """Menu item that allows the user to initialize/reset the database."""
 import subprocess
-from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from getch import pause
@@ -8,28 +7,30 @@ from getch import pause
 from vigorish.cli.menu_item import MenuItem
 from vigorish.cli.prompts import prompt_user_yes_no
 from vigorish.cli.util import print_message
+from vigorish.config.project_paths import NIGHTMAREJS_FOLDER, NODEJS_INBOX
 from vigorish.constants import EMOJI_DICT
 from vigorish.util.result import Result
 from vigorish.util.sys_helpers import node_is_installed, node_modules_folder_exists, run_command
 
-APP_FOLDER = Path(__file__).parent.parent.parent
-NIGHTMAREJS_FOLDER = APP_FOLDER.joinpath("nightmarejs").resolve()
-NODEJS_INBOX = NIGHTMAREJS_FOLDER.joinpath("inbox").resolve()
 INSTALL_ERROR = "Error! Node.js is not installed, see README for install instructions."
+INSTALL_HEADER = "You must install all required node packages in order to scrape any data.\n"
 INSTALL_MESSAGE = (
-    "Nightmare is not installed, you must install it and other node dependencies in order to "
-    "scrape any data."
+    "Select YES install Nightmare, Xfvb, and other node packages\n"
+    "Select NO to return to the previous menu"
 )
+UPDATE_HEADER = "Nightmare and all node dependencies are installed.\n"
 UPDATE_MESSAGE = (
-    "Nightmare and all node dependencies are installed. Select YES to check for updates and "
-    "install any new versions."
+    "Select YES to update all installed packages to the newest version.\n"
+    "Select NO to return to the previous menu."
 )
 
 
 class NpmInstallUpdate(MenuItem):
     def __init__(self, app):
         super().__init__(app)
-        self.menu_item_text = "Node Packages"
+        self.menu_item_text = (
+            "Install Node Packages" if not node_is_installed() else "Update Node Packages"
+        )
         self.menu_item_emoji = EMOJI_DICT.get("PACKAGE")
 
     def launch(self):
@@ -41,15 +42,17 @@ class NpmInstallUpdate(MenuItem):
         if not NODEJS_INBOX.exists():
             NODEJS_INBOX.mkdir(parents=True, exist_ok=True)
         if node_modules_folder_exists():
+            header = UPDATE_HEADER
             prompt = UPDATE_MESSAGE
             temp_folder = None
             command = "npm update --timeout=9999999"
         else:
+            header = INSTALL_HEADER
             prompt = INSTALL_MESSAGE
             temp_folder = TemporaryDirectory(dir=NIGHTMAREJS_FOLDER)
             command = f"npm install --timeout=9999999 --cache={temp_folder.name}"
-        print_message(prompt)
-        if not prompt_user_yes_no("Would you like to continue?"):
+        print_message(header, wrap=False, fg="bright_yellow", bold=True, underline=True)
+        if not prompt_user_yes_no(prompt):
             return Result.Ok(self.exit_menu)
         subprocess.run(["clear"])
         for line in run_command(command, cwd=str(NIGHTMAREJS_FOLDER)):
