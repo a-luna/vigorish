62ee25a5ae69b069327aa20603aa0e429ff26b98
Aaron Luna <admin@aaronluna.dev>
2020-09-11 15:35:38 -0700

    incorporate view models with config.database module

--- src/vigorish/config/database.py
+++ src/vigorish/config/database.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+from vigorish.config.project_paths import VIG_FOLDER
 from vigorish.models.boxscore import Boxscore
 from vigorish.models.game_bat_stats import GameBatStats
 from vigorish.models.game_event import GameEvent
@@ -18,6 +19,7 @@ from vigorish.models.game_substitution import GameSubstitution
 from vigorish.models.game_team_totals import GameTeamTotals
 from vigorish.models.player import Player
 from vigorish.models.player_id import PlayerId
+from vigorish.models.pitchfx import PitchFx
 from vigorish.models.runners_on_base import RunnersOnBase
 from vigorish.models.scrape_job import ScrapeJob
 from vigorish.models.scrape_error import ScrapeError
@@ -26,12 +28,17 @@ from vigorish.models.status_date import DateScrapeStatus
 from vigorish.models.status_game import GameScrapeStatus
 from vigorish.models.status_pitch_appearance import PitchAppScrapeStatus
 from vigorish.models.team import Team
+from vigorish.models.views.date_pitch_app_view import Date_PitchApp_View
+from vigorish.models.views.game_pitch_app_view import Game_PitchApp_View
+from vigorish.models.views.season_date_view import Season_Date_View
+from vigorish.models.views.season_game_view import Season_Game_View
+from vigorish.models.views.season_pitch_app_view import Season_PitchApp_View
+from vigorish.models.views.season_game_pitch_app_view import Season_Game_PitchApp_View
 from vigorish.setup.populate_tables import populate_tables
 
 
-VIG_FOLDER = Path.home() / ".vig"
-SQLITE_DEV_URL = f"sqlite:///{VIG_FOLDER / 'vig_dev.db'}"
-SQLITE_PROD_URL = f"sqlite:///{VIG_FOLDER / 'vig.db'}"
+SQLITE_DEV_URL = f"sqlite:///{VIG_FOLDER.joinpath('vig_dev.db')}"
+SQLITE_PROD_URL = f"sqlite:///{VIG_FOLDER.joinpath('vig.db')}"
 
 
 def get_db_url():
@@ -42,10 +49,10 @@ def get_db_url():
     return db_url if db_url else SQLITE_DEV_URL if env == "dev" else SQLITE_PROD_URL
 
 
-def initialize_database(db_engine, db_session):
-    Base.metadata.drop_all(db_engine)
-    Base.metadata.create_all(db_engine)
-    return populate_tables(db_session)
+def initialize_database(app):
+    Base.metadata.drop_all(app["db_engine"])
+    Base.metadata.create_all(app["db_engine"])
+    return populate_tables(app)
 
 
 def db_setup_complete(db_engine, db_session):
