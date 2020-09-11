170299b36a1584d4c588eda3e6e4af0f2de6ae6e
Aaron Luna <admin@aaronluna.dev>
2020-09-11 15:36:48 -0700

    add CLI command to sync local folder and s3 bucket

--- src/vigorish/cli/vig.py
+++ src/vigorish/cli/vig.py
@@ -1,6 +1,5 @@
 """CLI application entry point."""
 import subprocess
-from pathlib import Path
 
 import click
 from sqlalchemy import create_engine
@@ -12,43 +11,46 @@ from vigorish.cli.util import print_message, validate_scrape_dates
 from vigorish.config.dotenv_file import DotEnvFile
 from vigorish.config.database import get_db_url, initialize_database, ScrapeJob
 from vigorish.config.config_file import ConfigFile
-from vigorish.constants import DATA_SET_NAMES_SHORT
+from vigorish.config.project_paths import VIG_FOLDER
+from vigorish.constants import DATA_SET_NAME_MAP, FILE_TYPE_NAME_MAP
 from vigorish.data.scraped_data import ScrapedData
-from vigorish.enums import StatusReport
+from vigorish.enums import StatusReport, DataSet, VigFile, SyncDirection
 from vigorish.scrape.job_runner import JobRunner
 from vigorish.status.report_status import (
     report_status_single_date,
     report_date_range_status,
     report_season_status,
 )
+from vigorish.tasks.sync_data_no_prompts import SyncScrapedDataNoPrompts
 from vigorish.util.datetime_util import today_str, current_year
-
-VIG_FOLDER = Path.home().joinpath(".vig")
+from vigorish.util.result import Result
 
 # TODO: Add Refresh Game Data menu option
 # TODO: New Status Report menu option - Single Game. User can provide either BB or BR ID
 # TODO: Another Status Report option - Pitch Appearance. Simply call .display() method
-# TODO: New Bulk Download menu option - Get all HTML, JSON, or BOTH from S3 for a single season
-#       Figure out what files are NOT already present in local folders, only download missing
 # TODO: Eventually, will need menu option to edit season dates and add new seasons
 
 
 @click.group()
 @click.pass_context
 def cli(ctx):
-    """Entry point for the CLI application."""
+    """
+    Vigorish scrapes various websites for MLB data.
+
+    Please visit aaronluna.dev/projects/vigorish for user guides and project documentation.
+    """
     if not VIG_FOLDER.exists():
         VIG_FOLDER.mkdir(parents=True, exist_ok=True)
     dotenv = DotEnvFile()
     config = ConfigFile()
-    engine = create_engine(get_db_url())
-    session_maker = sessionmaker(bind=engine)
+    db_engine = create_engine(get_db_url())
+    session_maker = sessionmaker(bind=db_engine)
     db_session = session_maker()
-    scraped_data = ScrapedData(db_session=db_session, config=config)
+    scraped_data = ScrapedData(db_engine, db_session, config)
     ctx.obj = {
         "dotenv": dotenv,
         "config": config,
-        "engine": engine,
+        "db_engine": db_engine,
         "db_session": db_session,
         "scraped_data": scraped_data,
     }
@@ -76,7 +78,7 @@ def setup(app):
     deleted. This cannot be undone.
     """
     print()  # place an empty line between the command and the progress bars
-    result = initialize_database(app["engine"], app["db_session"])
+    result = initialize_database(app)
     if result.failure:
         return exit_app_error(app, result.error)
     return exit_app_success(app, "Successfully populated database with initial data.")
@@ -85,7 +87,7 @@ def setup(app):
 @cli.command()
 @click.option(
     "--data-set",
-    type=click.Choice(DATA_SET_NAMES_SHORT.keys()),
+    type=click.Choice(DATA_SET_NAME_MAP.keys()),
     multiple=True,
     default=["all"],
     show_default=True,
@@ -123,7 +125,7 @@ def setup(app):
 @click.pass_obj
 def scrape(app, data_set, start, end, name):
     """Scrape MLB data from websites."""
-    data_sets_int = sum(int(DATA_SET_NAMES_SHORT[ds]) for ds in data_set)
+    data_sets_int = sum(int(DATA_SET_NAME_MAP[ds]) for ds in data_set)
     result = validate_scrape_dates(app["db_session"], start, end)
     if result.failure:
         return exit_app_error(app, result.error)
@@ -268,6 +270,61 @@ def status_season(app, year, verbosity):
         error = "Unknown error occurred, unable to display status report."
         return exit_app_error(app, error)
     result = report_season_status(app["db_session"], year, report_type)
+    return exit_app_success(app) if result.success else exit_app_error(app, result.error)
+
+
+@cli.group()
+@click.pass_obj
+def sync(app):
+    """Synchronize scraped data to/from S3 bucket."""
+    pass
+
+
+@sync.command("up")
+@click.argument("year", type=MlbSeason(), default=current_year)
+@click.option(
+    "--file-type",
+    type=click.Choice([str(ft) for ft in VigFile if ft != VigFile.ALL]),
+    help="Type of file to sync, must provide only one value.",
+)
+@click.option(
+    "--data-sets",
+    type=click.Choice([str(ds) for ds in DataSet]),
+    multiple=True,
+    help="Data set(s) to sync, multiple values can be provided.",
+)
+@click.pass_obj
+def sync_up_to_s3(app, year, file_type, data_sets):
+    """Sync files from local folder to S3 bucket."""
+    file_type = FILE_TYPE_NAME_MAP.get(file_type, None)
+    data_sets_int = sum(int(DATA_SET_NAME_MAP[ds]) for ds in data_sets)
+    sync_task = SyncScrapedDataNoPrompts(app)
+    result_dict = sync_task.execute(SyncDirection.UP_TO_S3, year, file_type, data_sets_int)
+    result = Result.Combine([result for result in result_dict.values()])
+    return exit_app_success(app) if result.success else exit_app_error(app, result.error)
+
+
+@sync.command("down")
+@click.argument("year", type=MlbSeason(), default=current_year)
+@click.option(
+    "--file-type",
+    type=click.Choice([str(ft) for ft in VigFile if ft != VigFile.ALL]),
+    help="Type of file to sync, must provide only one value.",
+)
+@click.option(
+    "--data-sets",
+    type=click.Choice([str(ds) for ds in DataSet]),
+    multiple=True,
+    help="Data set(s) to sync, multiple values can be provided.",
+)
+@click.pass_obj
+def sync_down_to_local(app, year, file_type, data_sets):
+    """Sync files from S3 bucket to local folder."""
+    file_type = FILE_TYPE_NAME_MAP.get(file_type, None)
+    data_sets_int = sum(int(DATA_SET_NAME_MAP[ds]) for ds in data_sets)
+    sync_task = SyncScrapedDataNoPrompts(app)
+    result_dict = sync_task.execute(SyncDirection.DOWN_TO_LOCAL, year, file_type, data_sets_int)
+    result = Result.Combine([result for result in result_dict.values()])
     return exit_app_success(app) if result.success else exit_app_error(app, result.error)
 
 
