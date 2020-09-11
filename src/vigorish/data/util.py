e759cb8bf71f9193594184413a551e3b64d641a9
Aaron Luna <admin@aaronluna.dev>
2020-09-11 15:33:20 -0700

    refactored data.util module

--- src/vigorish/data/util.py
+++ src/vigorish/data/util.py
@@ -2,36 +2,59 @@ from datetime import datetime, timezone
 
 from vigorish.util.datetime_util import TIME_ZONE_NEW_YORK
 from vigorish.util.regex import PFX_TIMESTAMP_REGEX
+from vigorish.util.numeric_helpers import trim_data_set
 from vigorish.util.string_helpers import validate_bbref_game_id
 
 
 def calc_pitch_metrics(combined_data):
     pitch_samples = []
+    at_bat_samples = []
     inning_samples = []
+    prev_at_bat_last_pfx = None
     prev_inning_last_pfx = None
     for half_inning in combined_data["play_by_play_data"]:
         for ab_num, at_bat in enumerate(half_inning["inning_events"], start=1):
-            if at_bat["pitchfx_data_error"] or at_bat["missing_pitchfx_count"]:
+            if (
+                at_bat["at_bat_pitchfx_audit"]["pitchfx_error"]
+                or at_bat["at_bat_pitchfx_audit"]["missing_pitchfx_count"]
+            ):
                 continue
             for count, pfx in enumerate(at_bat["pitchfx"], start=1):
                 if count == 1 and prev_inning_last_pfx and ab_num == 1:
-                    this_pitch = get_time_pitch_thrown(pfx)
-                    last_pitch = get_time_pitch_thrown(prev_inning_last_pfx)
-                    time_between_innings = this_pitch - last_pitch
-                    if time_between_innings.total_seconds() > 0:
-                        inning_samples.append(time_between_innings.total_seconds())
-                if count == len(at_bat["pitchfx"]) and ab_num == len(half_inning["inning_events"]):
-                    prev_inning_last_pfx = pfx
+                    seconds_between_innings = get_seconds_between_pitches(prev_inning_last_pfx, pfx)
+                    if seconds_between_innings > 0:
+                        inning_samples.append(seconds_between_innings)
+                    prev_inning_last_pfx = None
+                if count == 1 and prev_at_bat_last_pfx and ab_num != 1:
+                    seconds_between_at_bats = get_seconds_between_pitches(prev_at_bat_last_pfx, pfx)
+                    if seconds_between_at_bats > 0:
+                        at_bat_samples.append(seconds_between_at_bats)
+                    prev_at_bat_last_pfx = None
+                if count == len(at_bat["pitchfx"]):
+                    if ab_num == len(half_inning["inning_events"]):
+                        prev_inning_last_pfx = pfx
+                    else:
+                        prev_at_bat_last_pfx = pfx
                     continue
-                next_pitch = get_time_pitch_thrown(at_bat["pitchfx"][count])
-                this_pitch = get_time_pitch_thrown(pfx)
-                time_between_pitches = next_pitch - this_pitch
-                if time_between_pitches.total_seconds() > 0:
-                    pitch_samples.append(time_between_pitches.total_seconds())
+                seconds_between_pitches = get_seconds_between_pitches(pfx, at_bat["pitchfx"][count])
+                if seconds_between_pitches > 0:
+                    pitch_samples.append(seconds_between_pitches)
+    return (
+        pitch_samples,
+        process_data_set(pitch_samples),
+        at_bat_samples,
+        process_data_set(at_bat_samples),
+        inning_samples,
+        process_data_set(inning_samples),
+    )
 
-    pitch_metrics = process_data_set(pitch_samples)
-    inning_metrics = process_data_set(inning_samples)
-    return (pitch_samples, pitch_metrics, inning_samples, inning_metrics)
+
+def get_seconds_between_pitches(pitch1, pitch2):
+    pitch1_thrown = get_time_pitch_thrown(pitch1)
+    pitch2_thrown = get_time_pitch_thrown(pitch2)
+    if not pitch1_thrown or not pitch2_thrown:
+        return 0
+    return (pitch2_thrown - pitch1_thrown).total_seconds()
 
 
 def get_time_pitch_thrown(pfx):
@@ -40,25 +63,34 @@ def get_time_pitch_thrown(pfx):
         return None
     group_dict = match.groupdict()
     game_dict = validate_bbref_game_id(pfx["bbref_game_id"]).value
-    timestamp = datetime(
-        game_dict["game_date"].year,
-        int(group_dict["month"]),
-        int(group_dict["day"]),
-        int(group_dict["hour"]),
-        int(group_dict["minute"]),
-        int(group_dict["second"]),
-    )
+    try:
+        timestamp = datetime(
+            game_dict["game_date"].year,
+            int(group_dict["month"]),
+            int(group_dict["day"]),
+            int(group_dict["hour"]),
+            int(group_dict["minute"]),
+            int(group_dict["second"]),
+        )
+    except ValueError:
+        return None
     return timestamp.replace(tzinfo=timezone.utc).astimezone(TIME_ZONE_NEW_YORK)
 
 
 def process_data_set(data_set, trim=False, st_dev=1):
     if not data_set or not isinstance(data_set, list):
         return {}
-    return {
+    if trim:
+        data_set = trim_data_set(data_set, st_dev_limit=st_dev)
+    results = {
         "total": sum(data_set),
         "count": len(data_set),
         "avg": sum(data_set) / len(data_set),
         "max": max(data_set),
         "min": min(data_set),
         "range": max(data_set) - min(data_set),
+        "trim": trim,
     }
+    if trim:
+        results["trim_stdev"] = st_dev
+    return results
