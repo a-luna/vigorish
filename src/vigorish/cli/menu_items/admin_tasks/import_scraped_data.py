bcc43390613e76988fa74c194df9d43698f93f8a
Aaron Luna <admin@aaronluna.dev>
2020-10-08 00:01:19 -0700

    replace prior implementations with new viewer components

--- src/vigorish/cli/menu_items/admin_tasks/import_scraped_data.py
+++ src/vigorish/cli/menu_items/admin_tasks/import_scraped_data.py
@@ -1,11 +1,36 @@
 """Update bbref_player_id_map.json file."""
+import subprocess
+
 from halo import Halo
 
 from vigorish.cli.menu_item import MenuItem
-from vigorish.cli.util import get_random_cli_color, get_random_dots_spinner
-from vigorish.constants import EMOJI_DICT
+from vigorish.cli.components import (
+    yes_no_prompt,
+    user_options_prompt,
+    get_random_cli_color,
+    get_random_dots_spinner,
+    print_message,
+)
+from vigorish.constants import EMOJI_DICT, MENU_NUMBERS
 from vigorish.tasks.import_scraped_data import ImportScrapedDataInLocalFolder
 from vigorish.util.result import Result
+
+IMPORT_DATA_MESSAGE = (
+    "This task uses the value of the JSON_LOCAL_FOLDER_PATH config setting to locate scraped "
+    "data in the local file system from all data sets and all MLB seasons. After the search is "
+    "complete, the scraped data is used to update the database."
+)
+IMPORT_DATA_PROMPT = "Select YES to run this task\nSelect NO to return to the previous menu"
+OVERWRITE_DATA_MESSAGE = (
+    'By default, data that is already considered "scraped" within the database will not be '
+    "updated with the data found in the local file system."
+)
+OVERWRITE_DATA_PROMPT = (
+    "Select KEEP EXISTING DATA to use the default behavior and only update data which is not "
+    'already marked as "scraped" in the database\n'
+    "Select OVERWRITE EXISTING DATA to update all data in the database with the values found in "
+    "the local file system"
+)
 
 
 class ImportScrapedDataTask(MenuItem):
@@ -17,24 +42,39 @@ class ImportScrapedDataTask(MenuItem):
         self.menu_item_emoji = EMOJI_DICT.get("HONEY_POT")
 
     def launch(self):
-        result = self.import_scraped_data.execute(overwrite_existing=True)
+        if not self.prompt_user_import_data():
+            return Result.Ok(True)
+        self.subscribe_to_events()
+        result = self.prompt_user_overwrite_data()
+        if result.failure:
+            return Result.Ok(True)
+        overwrite_existing_data = result.value
+        result = self.import_scraped_data.execute(overwrite_existing_data)
+        self.unsubscribe_from_events()
         if result.failure:
             return result
         return Result.Ok(True)
+
+    def prompt_user_import_data(self):
+        subprocess.run(["clear"])
+        print_message(IMPORT_DATA_MESSAGE, fg="bright_yellow", bold=True)
+        return yes_no_prompt(IMPORT_DATA_PROMPT, wrap=False)
+
+    def prompt_user_overwrite_data(self):
+        subprocess.run(["clear"])
+        print_message(OVERWRITE_DATA_MESSAGE, fg="bright_yellow", bold=True)
+        choices = {
+            f"{MENU_NUMBERS.get(1)}  KEEP EXISTING DATA": False,
+            f"{MENU_NUMBERS.get(2)}  OVERWRITE EXISTING DATA": True,
+            f"{EMOJI_DICT.get('BACK')} Return to Admin/Tasks Menu": None,
+        }
+        return user_options_prompt(choices, OVERWRITE_DATA_PROMPT, clear_screen=False)
 
     def error_occurred(self, error_message, data_set, year):
         self.spinner.fail(f"Error occurred while updating {data_set} for MLB {year}")
 
     def subscribe_to_events(self):
         self.import_scraped_data.events.error_occurred += self.error_occurred
-        self.import_scraped_data.events.no_scraped_data_found += self.no_scraped_data_found
-        self.import_scraped_data.events.remove_invalid_pitchfx_logs_start += (
-            self.remove_invalid_pitchfx_logs_start
-        )
-        self.import_scraped_data.events.remove_invalid_pitchfx_logs_complete += (
-            self.remove_invalid_pitchfx_logs_complete
-        )
-        self.import_scraped_data.events.import_scraped_data_start += self.import_scraped_data_start
-        self.import_scraped_data.events.import_scraped_data_complete += (
-            self.import_scraped_data_complete
-        )
+
+    def unsubscribe_from_events(self):
+        self.import_scraped_data.events.error_occurred -= self.error_occurred
