bb6f2326feba7e35a67b666d94545aa62a69efd2
Aaron Luna <admin@aaronluna.dev>
2020-10-11 03:56:59 -0700

    test case now passes when standalone
    -previously, this test case depended on the combined_data test case having ran before it

--- tests/test_task_calculate_avg_pitch_times.py
+++ tests/test_task_calculate_avg_pitch_times.py
@@ -1,7 +1,27 @@
+from vigorish.status.update_status_combined_data import update_pitch_apps_for_game_combined_data
 from vigorish.tasks.calculate_avg_pitch_times import CalculateAverageTimeBetweenPitches
+
+from tests.test_combine_scraped_data import (
+    combine_scraped_data_for_game,
+    GAME_ID_NO_ERRORS,
+    GAME_ID_PATCH_PFX,
+)
+from tests.util import revert_pitch_logs_to_state_before_combined_data
 
 
 def test_task_calculate_avg_pitch_times(vig_app):
+    db_session = vig_app["db_session"]
+    scraped_data = vig_app["scraped_data"]
+    combined_data_1 = combine_scraped_data_for_game(db_session, scraped_data, GAME_ID_NO_ERRORS)
+    assert combined_data_1
+    result = update_pitch_apps_for_game_combined_data(db_session, combined_data_1)
+    assert result.success
+    combined_data_2 = combine_scraped_data_for_game(
+        db_session, scraped_data, GAME_ID_PATCH_PFX, apply_patch_list=True
+    )
+    assert combined_data_2
+    result = update_pitch_apps_for_game_combined_data(db_session, combined_data_2)
+    assert result.success
     calc_pitch_times = CalculateAverageTimeBetweenPitches(vig_app)
     result = calc_pitch_times.execute(trim_data_sets=False)
     assert result.success
@@ -36,3 +56,12 @@ def test_task_calculate_avg_pitch_times(vig_app):
     assert time_between_innings["max"] == 240
     assert time_between_innings["min"] == 126
     assert time_between_innings["range"] == 114
+
+    result = revert_pitch_logs_to_state_before_combined_data(
+        db_session, scraped_data, GAME_ID_NO_ERRORS
+    )
+    assert result.success
+    result = revert_pitch_logs_to_state_before_combined_data(
+        db_session, scraped_data, GAME_ID_PATCH_PFX
+    )
+    assert result.success
