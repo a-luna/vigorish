647cdbf3132295a596a112a6f659e4fe359b52a2
Aaron Luna <contact@aaronluna.dev>
2020-12-03 19:34:29 -0800

    calculating pitch mix/metrics is greatly simplified (#571)

--- src/vigorish/util/pitch_calcs.py
+++ src/vigorish/util/pitch_calcs.py
@@ -1,29 +1,40 @@
-from copy import deepcopy
-
 from vigorish.enums import PitchType
 
+PITCH_METRIC_NAMES = ["total_pitches", "avg_speed", "avg_pfx_x", "avg_pfx_z", "avg_px", "avg_pz"]
 
-def calc_pitch_mix(pitch_count_dict, threshold=0.01):
-    pitch_types = []
-    mix_total = 0
-    for col_name, val in pitch_count_dict.items():
-        if not ("_count" in col_name):
-            continue
-        if val == 0:
-            continue
-        if not _check_threshold(val, pitch_count_dict["total_pitches"], threshold):
-            continue
-        mix_total += val
-        pitch_type_abbrev = col_name.split("_")[0].upper()
-        pitch_type = PitchType.from_abbrev(pitch_type_abbrev)
-        pitch_types.append({"type": pitch_type, "count": val})
+
+def get_metrics_for_all_pitch_types(pitch_type_results, threshold=0.01):
+    all_pitch_types = {}
+    for d in [dict(row) for row in pitch_type_results]:
+        (pitch_type, metrics) = get_metrics_for_single_pitch_type(d)
+        all_pitch_types[pitch_type] = metrics
+    total_pitches_all = sum(metrics["total_pitches"] for metrics in all_pitch_types.values())
+    total_pitches_valid = 0
     pitch_mix_int = 0
-    for pt in pitch_types:
-        pt["percent"] = round(pt["count"] / float(mix_total), 3)
-        pitch_mix_int += int(pt["type"])
-    pitch_types.append({"type": PitchType.ALL, "percent": 1.000, "count": mix_total})
-    pitch_types.sort(key=lambda x: x["percent"], reverse=True)
-    return {x["type"]: {"percent": x["percent"], "count": x["count"]} for x in pitch_types}
+    valid_pitch_types = []
+    for pitch_type, metrics in all_pitch_types.items():
+        if not _check_threshold(metrics["total_pitches"], total_pitches_all, threshold):
+            continue
+        total_pitches_valid += metrics["total_pitches"]
+        pitch_mix_int += int(pitch_type)
+        metrics["pitch_type"] = pitch_type
+        metrics["count"] = metrics.pop("total_pitches")
+        valid_pitch_types.append(metrics)
+    for metrics in valid_pitch_types:
+        metrics["percent"] = round(metrics["count"] / total_pitches_valid, 3)
+    valid_pitch_types.sort(key=lambda x: x["percent"], reverse=True)
+    pitch_mix = get_pitch_types_from_int(pitch_mix_int)
+    pitch_mix_total = {"type": pitch_mix, "percent": 1.000, "count": total_pitches_valid}
+    pitch_mix_detail = {x["pitch_type"]: x for x in valid_pitch_types}
+    return (pitch_mix_total, pitch_mix_detail)
+
+
+def get_metrics_for_single_pitch_type(pitch_type_dict):
+    pitch_metrics = {}
+    pitch_type = PitchType.from_abbrev(pitch_type_dict["pitch_type"])
+    for metric in PITCH_METRIC_NAMES:
+        pitch_metrics[metric] = round(pitch_type_dict[metric], 3)
+    return (pitch_type, pitch_metrics)
 
 
 def _check_threshold(pitch_count, total_pitches, threshold):
@@ -31,23 +42,11 @@ def _check_threshold(pitch_count, total_pitches, threshold):
     return percent >= threshold
 
 
-def get_pitch_mix_from_int(pitch_mix_int):
+def get_pitch_types_from_int(pitch_mix_int):
     pitch_types = []
-    for ptype in PitchType:
-        if ptype == PitchType.ALL:
+    for pt in PitchType:
+        if pt == PitchType.ALL:
             continue
-        if pitch_mix_int & ptype == ptype:
-            pitch_types.append(ptype)
+        if pitch_mix_int & pt == pt:
+            pitch_types.append(pt)
     return pitch_types
-
-
-def get_pitch_type_metrics(pitch_type_dict):
-    d = deepcopy(pitch_type_dict)
-    d.pop("id")
-    d.pop("season_id")
-    d.pop("mlb_id")
-    d.pop("name")
-    pitch_type = PitchType.from_abbrev(d.pop("pitch_type"))
-    for metric, val in d.items():
-        d[metric] = round(val, 3)
-    return (pitch_type, d)
