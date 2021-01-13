d86a5b48a2903cd0e5747ed5c44bcbfd14e9857e
Aaron Luna <contact@aaronluna.dev>
2021-01-12 16:31:06 -0800

    refactor vigorish.tasks.update_player_maps module (#601)

--- src/vigorish/util/dataclass_helpers.py
+++ src/vigorish/util/dataclass_helpers.py
@@ -1,34 +1,52 @@
+from dataclasses import asdict
 from datetime import datetime
 
-from vigorish.util.dt_format_strings import DATE_ONLY, DT_AWARE
+from dacite import from_dict
+
+from vigorish.util.dt_format_strings import DATE_ONLY
 
 
-def dict_from_dataclass(obj, data_class, date_format=DT_AWARE):
-    obj_dict = {}
-    for name, field in data_class.__dataclass_fields__.items():
-        value = getattr(obj, name, None)
-        if field.type is int:
-            obj_dict[name] = int(value)
+def serialize_data_class_to_csv(data_class_objects, date_format):
+    dataclass_dicts = [asdict(do) for do in data_class_objects]
+    if not dataclass_dicts:
+        return None
+    col_names = [",".join(list(dataclass_dicts[0].keys()))]
+    csv_rows = [dict_to_csv_row(d, date_format) for d in dataclass_dicts]
+    return "\n".join((col_names + csv_rows))
+
+
+def deserialize_data_class_from_csv(csv_text, data_class):
+    csv_rows = csv_text.split("\n")
+    col_names = [col.strip() for col in csv_rows.pop(0).split(",")]
+    csv_rows = [row.split(",") for row in csv_rows]
+    csv_dict_list = [dict(zip(col_names, row)) for row in csv_rows if row != [""]]
+    return [from_dict(data_class=data_class, data=csv_dict) for csv_dict in csv_dict_list]
+
+
+def serialize_db_object_to_csv(db_obj, dataclass, date_format=DATE_ONLY):
+    csv_dict = {}
+    for name, field in dataclass.__dataclass_fields__.items():
+        value = getattr(db_obj, name, None)
+        if field.type is int and not isinstance(value, int):
+            csv_dict[name] = int(value)
+        elif field.type is float and not isinstance(value, float):
+            csv_dict[name] = float(value)
         elif field.type is bool:
-            obj_dict[name] = True if value else False
+            csv_dict[name] = True if value else False
         elif not value:
-            obj_dict[name] = None
-        elif field.type is datetime:
-            obj_dict[name] = value.strftime(date_format)
-        elif field.type is float:
-            obj_dict[name] = float(value)
+            csv_dict[name] = None
         else:
-            obj_dict[name] = value
-    return obj_dict
+            csv_dict[name] = value
+    return dict_to_csv_row(csv_dict, date_format)
 
 
-def sanitize_row_dict(row_dict, date_format=DATE_ONLY):
-    return [val_to_string(val, date_format) for val in row_dict.values()]
+def dict_to_csv_row(csv_dict, date_format):
+    return ",".join([sanitize_value_for_csv(val, date_format) for val in csv_dict.values()])
 
 
-def val_to_string(val, date_format):
+def sanitize_value_for_csv(val, date_format):
     return (
-        val
+        val.replace(",", ";")
         if isinstance(val, str)
         else val.strftime(date_format).strip()
         if isinstance(val, datetime)
