e0855d3f65fd39c26736d83d4dd15a3569ca55f1
Aaron Luna <contact@aaronluna.dev>
2021-01-12 16:16:10 -0800

    refactor scrape_mlb_player_info task

--- src/vigorish/tasks/scrape_mlb_player_info.py
+++ src/vigorish/tasks/scrape_mlb_player_info.py
@@ -2,9 +2,11 @@
 from datetime import date, datetime, timedelta
 from json.decoder import JSONDecodeError
 
+from events import Events
 from sqlalchemy.exc import DBAPIError, SQLAlchemyError
 
 from vigorish.database import Player
+from vigorish.tasks.base import Task
 from vigorish.util.request_url import request_url_with_retries
 from vigorish.util.result import Result
 from vigorish.util.string_helpers import fuzzy_match
@@ -13,115 +15,125 @@ MLB_PLAYER_SEARCH_URL = "http://lookup-service-prod.mlb.com/json/named.search_pl
 MLB_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 
-def scrape_mlb_player_info(db_session, name, bbref_id, game_date):
-    return (
-        get_search_url(name)
-        .on_success(request_url_with_retries)
-        .on_success(decode_json_response)
-        .on_success(get_player_data, name, game_date)
-        .on_success(parse_player_data, bbref_id, db_session)
-    )
+class ScrapeMlbPlayerInfoTask(Task):
+    def __init__(self, app):
+        super().__init__(app)
+        self.events = Events(
+            (
+                "error_occurred",
+                "scrape_player_info_start",
+                "scrape_player_info_complete",
+            )
+        )
 
+    def execute(self, name, bbref_id, game_date):
+        return (
+            self.get_search_url(name)
+            .on_success(request_url_with_retries)
+            .on_success(self.decode_json_response)
+            .on_success(self.get_player_data, name, game_date)
+            .on_success(self.parse_player_data, bbref_id)
+            .on_success(self.add_player_to_database)
+        )
 
-def get_search_url(name):
-    split = name.split()
-    if not split or len(split) <= 1:
-        return Result.Fail(f"Name was not in an expected format: {name}")
-    name_part = ("%20".join(split[1:])).upper()
-    url = f"{MLB_PLAYER_SEARCH_URL}?sport_code='mlb'&name_part='{name_part}%25'&active_sw='Y'"
-    return Result.Ok(url)
+    def get_search_url(self, name):
+        self.events.scrape_player_info_start(name)
+        split = name.split()
+        if not split or len(split) <= 1:  # pragma: no cover
+            return Result.Fail(f"Name was not in an expected format: {name}")
+        name_part = ("%20".join(split[1:])).upper()
+        url = f"{MLB_PLAYER_SEARCH_URL}?sport_code='mlb'&name_part='{name_part}%25'&active_sw='Y'"
+        return Result.Ok(url)
 
+    def decode_json_response(self, response):
+        query_results = ""
+        try:
+            resp_json = response.json()
+            query_results = resp_json["search_player_all"]["queryResults"]
+            num_results = int(query_results["totalSize"])
+            return Result.Ok((query_results, num_results))
+        except (JSONDecodeError, KeyError) as e:  # pragma: no cover
+            error = f"Failed to decode HTTP response as JSON: {repr(e)}\n{e.response.text}"
+            return Result.Fail(error)
+        except ValueError:  # pragma: no cover
+            error = f"Failed to parse number of results from search response: {query_results}"
+            return Result.Fail(error)
 
-def decode_json_response(response):
-    query_results = ""
-    try:
-        resp_json = response.json()
-        query_results = resp_json["search_player_all"]["queryResults"]
-        num_results = int(query_results["totalSize"])
-        return Result.Ok((query_results, num_results))
-    except (JSONDecodeError, KeyError) as e:
-        error = f"Failed to decode HTTP response as JSON: {repr(e)}\n{e.response.text}"
-        return Result.Fail(error)
-    except ValueError:
-        error = f"Failed to parse number of results from search response: {query_results}"
-        return Result.Fail(error)
+    def get_player_data(self, results_tuple, name, game_date):
+        results = results_tuple[0]
+        num_results = results_tuple[1]
+        if num_results > 1:
+            player_list = results["row"]
+            player_data = self.find_best_match(player_list, name, game_date)
+        else:
+            player_data = results["row"]
+        return Result.Ok(player_data)
 
+    def find_best_match(self, player_list, name, game_date):
+        player_id_name_map = {
+            player["player_id"]: player["name_display_first_last"] for player in player_list
+        }
+        player_info_dict = {player["player_id"]: player for player in player_list}
+        possible_matches = fuzzy_match(name, player_id_name_map)
+        if len(possible_matches) == 1:
+            return player_info_dict[possible_matches[0]["result"]]
+        return self.compare_mlb_debut(possible_matches, player_info_dict, game_date)
 
-def get_player_data(results_tuple, name, game_date):
-    results = results_tuple[0]
-    num_results = results_tuple[1]
-    if num_results > 1:
-        player_list = results["row"]
-        player_data = find_best_match(player_list, name, game_date)
-    else:
-        player_data = results["row"]
-    return Result.Ok(player_data)
+    def compare_mlb_debut(self, possible_matches, player_info_dict, game_date):
+        info_dict_list = [
+            player_info_dict[possible_match["result"]] for possible_match in possible_matches
+        ]
+        for player_info in info_dict_list:
+            if not player_info["pro_debut_date"]:  # pragma: no cover
+                player_info["since_debut"] = timedelta.max.days
+                continue
+            player_mlb_debut = datetime.strptime(player_info["pro_debut_date"], MLB_DATE_FORMAT)
+            player_info["since_debut"] = abs((game_date - player_mlb_debut.date()).days)
+        info_dict_list.sort(key=lambda x: x["since_debut"])
+        return info_dict_list[0]
 
+    def parse_player_data(self, player_data, bbref_id):
+        try:
+            feet = int(player_data["height_feet"])
+            inches = int(player_data["height_inches"])
+            height_total_inches = (feet * 12) + inches
+        except ValueError:  # pragma: no cover
+            height_total_inches = 0
 
-def find_best_match(player_list, name, game_date):
-    player_id_name_map = {
-        player["player_id"]: player["name_display_first_last"] for player in player_list
-    }
-    player_info_dict = {player["player_id"]: player for player in player_list}
-    match_results = fuzzy_match(name, player_id_name_map)
-    if len(match_results) == 1:
-        return player_info_dict[match_results[0]["result"]]
-    return compare_mlb_debut(match_results, player_info_dict, game_date)
+        try:
+            debut = datetime.strptime(player_data["pro_debut_date"], MLB_DATE_FORMAT).date()
+            birth_date = datetime.strptime(player_data["birth_date"], MLB_DATE_FORMAT).date()
+        except ValueError:  # pragma: no cover
+            debut = date.min
+            birth_date = date.min
 
+        player_dict = {
+            "name_first": player_data["name_first"],
+            "name_last": player_data["name_last"],
+            "name_given": player_data["name_first"],
+            "bats": player_data["bats"],
+            "throws": player_data["throws"],
+            "weight": player_data["weight"],
+            "height": height_total_inches,
+            "debut": debut,
+            "birth_year": birth_date.year,
+            "birth_month": birth_date.month,
+            "birth_day": birth_date.day,
+            "birth_country": player_data["birth_country"],
+            "birth_state": player_data["birth_state"],
+            "birth_city": player_data["birth_city"],
+            "bbref_id": bbref_id,
+            "mlb_id": player_data["player_id"],
+            "missing_mlb_id": False,
+        }
+        return Result.Ok(player_dict)
 
-def compare_mlb_debut(possible_matches, player_info_dict, game_date):
-    info_dict_list = [
-        player_info_dict[possible_match["result"]] for possible_match in possible_matches
-    ]
-    for player_info in info_dict_list:
-        if not player_info["pro_debut_date"]:
-            player_info["since_debut"] = timedelta.max.days
-            continue
-        player_mlb_debut = datetime.strptime(player_info["pro_debut_date"], MLB_DATE_FORMAT)
-        player_info["since_debut"] = abs((game_date - player_mlb_debut.date()).days)
-    info_dict_list.sort(key=lambda x: x["since_debut"])
-    return info_dict_list[0]
-
-
-def parse_player_data(player_info, bbref_id, db_session):
-    try:
-        feet = int(player_info["height_feet"])
-        inches = int(player_info["height_inches"])
-        height_total_inches = (feet * 12) + inches
-    except ValueError:
-        height_total_inches = 0
-
-    try:
-        debut = datetime.strptime(player_info["pro_debut_date"], MLB_DATE_FORMAT).date()
-        birth_date = datetime.strptime(player_info["birth_date"], MLB_DATE_FORMAT).date()
-    except ValueError:
-        debut = date.min
-        birth_date = date.min
-
-    player_dict = {
-        "name_first": player_info["name_first"],
-        "name_last": player_info["name_last"],
-        "name_given": player_info["name_first"],
-        "bats": player_info["bats"],
-        "throws": player_info["throws"],
-        "weight": player_info["weight"],
-        "height": height_total_inches,
-        "debut": debut,
-        "birth_year": birth_date.year,
-        "birth_month": birth_date.month,
-        "birth_day": birth_date.day,
-        "birth_country": player_info["birth_country"],
-        "birth_state": player_info["birth_state"],
-        "birth_city": player_info["birth_city"],
-        "bbref_id": bbref_id,
-        "mlb_id": player_info["player_id"],
-        "missing_mlb_id": False,
-    }
-
-    try:
-        new_player = Player(**player_dict)
-        db_session.add(new_player)
-        db_session.commit()
-        return Result.Ok(new_player)
-    except (SQLAlchemyError, DBAPIError) as e:
-        return Result.Fail(f"Error: {repr(e)}")
+    def add_player_to_database(self, player_dict):
+        try:
+            new_player = Player(**player_dict)
+            self.db_session.add(new_player)
+            self.db_session.commit()
+            self.events.scrape_player_info_complete(new_player)
+            return Result.Ok(new_player)
+        except (SQLAlchemyError, DBAPIError) as e:  # pragma: no cover
+            return Result.Fail(f"Error: {repr(e)}")
---
+++ tests/test_task_scrape_mlb_player_info.py
@@ -0,0 +1,267 @@
+from datetime import datetime
+
+from vigorish.tasks.scrape_mlb_player_info import ScrapeMlbPlayerInfoTask
+from vigorish.util.result import Result
+
+PLAYER_NAME = "Bryse Wilson"
+GAME_DATE = datetime(2018, 8, 20)
+BBREF_ID = "wilsobr02"
+MOCK_API_RESPONSE = {
+    "search_player_all": {
+        "queryResults": {
+            "created": "2021-01-09T08:24:28",
+            "totalSize": "7",
+            "row": [
+                {
+                    "position": "P",
+                    "birth_country": "Saudi Arabia",
+                    "weight": "227",
+                    "birth_state": "",
+                    "name_display_first_last": "Alex Wilson",
+                    "college": "Texas A&M",
+                    "height_inches": "0",
+                    "name_display_roster": "Wilson",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Alex",
+                    "team_code": "mil",
+                    "birth_city": "Dhahran",
+                    "height_feet": "6",
+                    "pro_debut_date": "2013-04-11T00:00:00",
+                    "team_full": "Milwaukee Brewers",
+                    "team_abbrev": "MIL",
+                    "birth_date": "1986-11-03T00:00:00",
+                    "throws": "R",
+                    "league": "NL",
+                    "name_display_last_first": "Wilson, Alex",
+                    "position_id": "1",
+                    "high_school": "Hurricane, WV",
+                    "name_use": "Alex",
+                    "player_id": "543935",
+                    "name_last": "Wilson",
+                    "team_id": "158",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "C",
+                    "birth_country": "USA",
+                    "weight": "230",
+                    "birth_state": "FL",
+                    "name_display_first_last": "Bobby Wilson",
+                    "college": "St. Petersburg JC, FL",
+                    "height_inches": "0",
+                    "name_display_roster": "Wilson, B",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Bobby",
+                    "team_code": "det",
+                    "birth_city": "Dunedin",
+                    "height_feet": "6",
+                    "pro_debut_date": "2008-04-28T00:00:00",
+                    "team_full": "Detroit Tigers",
+                    "team_abbrev": "DET",
+                    "birth_date": "1983-04-08T00:00:00",
+                    "throws": "R",
+                    "league": "AL",
+                    "name_display_last_first": "Wilson, Bobby",
+                    "position_id": "2",
+                    "high_school": "Seminole, FL",
+                    "name_use": "Bobby",
+                    "player_id": "435064",
+                    "name_last": "Wilson",
+                    "team_id": "116",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "P",
+                    "birth_country": "USA",
+                    "weight": "225",
+                    "birth_state": "NC",
+                    "name_display_first_last": "Bryse Wilson",
+                    "college": "",
+                    "height_inches": "2",
+                    "name_display_roster": "Wilson, Bry",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Bryse",
+                    "team_code": "atl",
+                    "birth_city": "Durham",
+                    "height_feet": "6",
+                    "pro_debut_date": "2018-08-20T00:00:00",
+                    "team_full": "Atlanta Braves",
+                    "team_abbrev": "ATL",
+                    "birth_date": "1997-12-20T00:00:00",
+                    "throws": "R",
+                    "league": "NL",
+                    "name_display_last_first": "Wilson, Bryse",
+                    "position_id": "1",
+                    "high_school": "Orange, Hillsborough, NC",
+                    "name_use": "Bryse",
+                    "player_id": "669060",
+                    "name_last": "Wilson",
+                    "team_id": "144",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "3B",
+                    "birth_country": "USA",
+                    "weight": "219",
+                    "birth_state": "TN",
+                    "name_display_first_last": "Jacob Wilson",
+                    "college": "Memphis",
+                    "height_inches": "11",
+                    "name_display_roster": "Wilson",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Jacob",
+                    "team_code": "oak",
+                    "birth_city": "Bartlett",
+                    "height_feet": "5",
+                    "pro_debut_date": "",
+                    "team_full": "Oakland Athletics",
+                    "team_abbrev": "OAK",
+                    "birth_date": "1990-07-29T00:00:00",
+                    "throws": "R",
+                    "league": "AL",
+                    "name_display_last_first": "Wilson, Jacob",
+                    "position_id": "5",
+                    "high_school": "Bartlett, TN",
+                    "name_use": "Jacob",
+                    "player_id": "607111",
+                    "name_last": "Wilson",
+                    "team_id": "133",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "P",
+                    "birth_country": "USA",
+                    "weight": "205",
+                    "birth_state": "CA",
+                    "name_display_first_last": "Justin Wilson",
+                    "college": "Fresno State",
+                    "height_inches": "2",
+                    "name_display_roster": "Wilson, J",
+                    "sport_code": "mlb",
+                    "bats": "L",
+                    "name_first": "Justin",
+                    "team_code": "nyn",
+                    "birth_city": "Anaheim",
+                    "height_feet": "6",
+                    "pro_debut_date": "2012-08-20T00:00:00",
+                    "team_full": "New York Mets",
+                    "team_abbrev": "NYM",
+                    "birth_date": "1987-08-18T00:00:00",
+                    "throws": "L",
+                    "league": "NL",
+                    "name_display_last_first": "Wilson, Justin",
+                    "position_id": "1",
+                    "high_school": "Dr. Floyd B. Buchanan, Clovis, CA",
+                    "name_use": "Justin",
+                    "player_id": "458677",
+                    "name_last": "Wilson",
+                    "team_id": "121",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "CF",
+                    "birth_country": "USA",
+                    "weight": "198",
+                    "birth_state": "CA",
+                    "name_display_first_last": "Marcus Wilson",
+                    "college": "",
+                    "height_inches": "2",
+                    "name_display_roster": "Wilson",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Marcus",
+                    "team_code": "bos",
+                    "birth_city": "Los Angeles",
+                    "height_feet": "6",
+                    "pro_debut_date": "",
+                    "team_full": "Boston Red Sox",
+                    "team_abbrev": "BOS",
+                    "birth_date": "1996-08-15T00:00:00",
+                    "throws": "R",
+                    "league": "AL",
+                    "name_display_last_first": "Wilson, Marcus",
+                    "position_id": "8",
+                    "high_school": "Junipero Serra, Gardena, CA",
+                    "name_use": "Marcus",
+                    "player_id": "657129",
+                    "name_last": "Wilson",
+                    "team_id": "111",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+                {
+                    "position": "C",
+                    "birth_country": "Venezuela",
+                    "weight": "245",
+                    "birth_state": "",
+                    "name_display_first_last": "Wilson Ramos",
+                    "college": "",
+                    "height_inches": "1",
+                    "name_display_roster": "Ramos, W",
+                    "sport_code": "mlb",
+                    "bats": "R",
+                    "name_first": "Wilson",
+                    "team_code": "nyn",
+                    "birth_city": "Valencia",
+                    "height_feet": "6",
+                    "pro_debut_date": "2010-05-02T00:00:00",
+                    "team_full": "New York Mets",
+                    "team_abbrev": "NYM",
+                    "birth_date": "1987-08-10T00:00:00",
+                    "throws": "R",
+                    "league": "NL",
+                    "name_display_last_first": "Ramos, Wilson",
+                    "position_id": "2",
+                    "high_school": "U.E. Santa Ines, Carabobo, VEN",
+                    "name_use": "Wilson",
+                    "player_id": "467092",
+                    "name_last": "Ramos",
+                    "team_id": "121",
+                    "service_years": "",
+                    "active_sw": "Y",
+                },
+            ],
+        },
+    }
+}
+
+
+class MockResponse:
+    @staticmethod
+    def json():
+        return MOCK_API_RESPONSE
+
+
+def test_scrape_mlb_player_info(vig_app, mocker):
+    def mock_request_url(url):
+        return Result.Ok(MockResponse())
+
+    mocker.patch("vigorish.tasks.scrape_mlb_player_info.request_url_with_retries", mock_request_url)
+    scrape_player_info_task = ScrapeMlbPlayerInfoTask(vig_app)
+    result = scrape_player_info_task.execute(PLAYER_NAME, BBREF_ID, GAME_DATE)
+    assert result.success
+    new_player = result.value
+    assert new_player.name_first == "Bryse"
+    assert new_player.name_last == "Wilson"
+    assert new_player.throws == "R"
+    assert new_player.bats == "R"
+    assert new_player.weight == 225
+    assert new_player.height == 74
+    assert new_player.debut == GAME_DATE
+    assert new_player.birth_year == 1997
+    assert new_player.birth_month == 12
+    assert new_player.birth_day == 20
+    assert new_player.birth_country == "USA"
+    assert new_player.birth_state == "NC"
+    assert new_player.birth_city == "Durham"
+    assert new_player.bbref_id == BBREF_ID
+    assert new_player.mlb_id == 669060
