ab14009819d21c213871959c0e1d5f69e9ed7feb
Aaron Luna <contact@aaronluna.dev>
2021-01-12 16:45:58 -0800

    refactor package import statements (#614)

---
+++ src/vigorish/models/views/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+from vigorish.models.views.date_pitch_app_view import Date_PitchApp_View
+from vigorish.models.views.game_pitch_app_view import Game_PitchApp_View
+from vigorish.models.views.pitch_app_pitchfx_view import (
+    PitchApp_PitchType_All_View,
+    PitchApp_PitchType_Left_View,
+    PitchApp_PitchType_Right_View,
+)
+from vigorish.models.views.player_pitchfx_view import (
+    Pitch_Type_All_View,
+    Pitch_Type_By_Year_View,
+    Pitch_Type_Left_View,
+    Pitch_Type_Right_View,
+)
+from vigorish.models.views.season_date_view import Season_Date_View
+from vigorish.models.views.season_game_pitch_app_view import Season_Game_PitchApp_View
+from vigorish.models.views.season_game_view import Season_Game_View
+from vigorish.models.views.season_pitch_app_view import Season_PitchApp_View
