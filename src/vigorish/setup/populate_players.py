5674b048dc8d9eac418cd6795d0e89cdb7295425
Aaron Luna <contact@aaronluna.dev>
2021-01-12 16:42:37 -0800

    add db table to associate players to teams (#611)

--- src/vigorish/setup/populate_players.py
+++ src/vigorish/setup/populate_players.py
@@ -5,8 +5,8 @@ from datetime import datetime
 from dataclass_csv import accept_whitespaces, DataclassReader, dateformat
 from tqdm import tqdm
 
-from vigorish.database import Player, PlayerId
-from vigorish.tasks.update_player_maps import UpdatePlayerIdMap
+from vigorish.database import Assoc_Player_Team, Player, PlayerId, Season, Team
+from vigorish.tasks.update_player_maps import UpdatePlayerIdMap, UpdatePlayerTeamMap
 from vigorish.util.dt_format_strings import DATE_ONLY
 from vigorish.util.result import Result
 
@@ -41,32 +41,36 @@ class PlayerCsvRow:
     bbrefID: str = None
 
 
-PLAYER_ID_MAP_FILENAME = "bbref_player_id_map.csv"
-PEOPLE_CSV_FILENAME = "People.csv"
+PLAYER_ID_MAP_CSV = "bbref_player_id_map.csv"
+PLAYER_TEAM_MAP_CSV = "bbref_player_team_map.csv"
+PEOPLE_CSV = "People.csv"
 
 
 def populate_players(app, csv_folder):
-    db_session = app.db_session
     """Populate player_id and player tables with initial data."""
-    result = import_idmap_csv(db_session, app, csv_folder)
+    result = import_id_map_csv(app, csv_folder)
     if result.failure:
         return result
-    db_session.commit()
-    result = import_people_csv(db_session, csv_folder)
+    app.db_session.commit()
+    result = import_people_csv(app.db_session, csv_folder)
     if result.failure:
         return result
-    db_session.commit()
+    app.db_session.commit()
+    result = import_team_map_csv(app, csv_folder)
+    if result.failure:
+        return result
+    app.db_session.commit()
     return Result.Ok()
 
 
-def import_idmap_csv(db_session, app, csv_folder):
+def import_id_map_csv(app, csv_folder):
     try:
         update_player_id_map = UpdatePlayerIdMap(app)
-        player_id_map_csv = csv_folder.joinpath(PLAYER_ID_MAP_FILENAME)
+        player_id_map_csv = csv_folder.joinpath(PLAYER_ID_MAP_CSV)
         player_id_map = update_player_id_map.read_bbref_player_id_map_from_file(player_id_map_csv)
         with tqdm(
             total=len(player_id_map),
-            desc="Populating player_id table...",
+            desc="Populating player_id table.....",
             unit="row",
             mininterval=0.12,
             maxinterval=5,
@@ -74,7 +78,7 @@ def import_idmap_csv(db_session, app, csv_folder):
             ncols=90,
         ) as pbar:
             for id_map in player_id_map:
-                db_session.add(
+                app.db_session.add(
                     PlayerId(
                         mlb_id=int(id_map.mlb_ID),
                         mlb_name=id_map.name_common,
@@ -86,12 +90,12 @@ def import_idmap_csv(db_session, app, csv_folder):
         return Result.Ok()
     except Exception as e:
         error = f"Error: {repr(e)}"
-        db_session.rollback()
+        app.db_session.rollback()
         return Result.Fail(error)
 
 
 def import_people_csv(db_session, csv_folder):
-    player_csv_file = csv_folder.joinpath(PEOPLE_CSV_FILENAME)
+    player_csv_file = csv_folder.joinpath(PEOPLE_CSV)
     csv_text = player_csv_file.read_text()
     total_rows = len([row for row in csv_text.split("\n") if row])
     try:
@@ -99,7 +103,7 @@ def import_people_csv(db_session, csv_folder):
             reader = DataclassReader(player_csv, PlayerCsvRow)
             with tqdm(
                 total=total_rows,
-                desc="Populating player table......",
+                desc="Populating player table........",
                 unit="row",
                 mininterval=0.12,
                 maxinterval=5,
@@ -134,10 +138,52 @@ def import_people_csv(db_session, csv_folder):
                         missing_mlb_id=False,
                     )
                     db_session.add(p)
+                    db_session.commit()
                     player_id.db_player_id = p.id
                     pbar.update()
         return Result.Ok()
     except Exception as e:
         error = f"Error: {repr(e)}"
         db_session.rollback()
+        return Result.Fail(error)
+
+
+def import_team_map_csv(app, csv_folder):
+    try:
+        update_player_team_map = UpdatePlayerTeamMap(app)
+        player_team_map_csv = csv_folder.joinpath(PLAYER_TEAM_MAP_CSV)
+        player_team_map = update_player_team_map.read_bbref_player_team_map_from_file(
+            player_team_map_csv
+        )
+        with tqdm(
+            total=len(player_team_map),
+            desc="Populating player_team table...",
+            unit="row",
+            mininterval=0.12,
+            maxinterval=5,
+            unit_scale=True,
+            ncols=90,
+        ) as pbar:
+            for team_map in player_team_map:
+                player = Player.find_by_bbref_id(app.db_session, team_map.player_ID)
+                team = Team.find_by_team_id_and_year(
+                    app.db_session, team_map.team_ID, int(team_map.year_ID)
+                )
+                season = Season.find_by_year(app.db_session, int(team_map.year_ID))
+                player_team = Assoc_Player_Team(
+                    db_player_id=player.id,
+                    db_team_id=team.id,
+                    mlb_id=int(team_map.mlb_ID),
+                    bbref_id=team_map.player_ID,
+                    team_id=team.team_id_br,
+                    year=int(team_map.year_ID),
+                    stint_number=team_map.stint_ID,
+                    season_id=season.id,
+                )
+                app.db_session.add(player_team)
+                pbar.update()
+        return Result.Ok()
+    except Exception as e:
+        error = f"Error: {repr(e)}"
+        app.db_session.rollback()
         return Result.Fail(error)
