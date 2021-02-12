f1ed66711e7cd61102a8c0b206ae43512026eff8
Aaron Luna <contact@aaronluna.dev>
2021-02-11 20:26:12 -0800

    fix: test case failing CI

--- tests/test_with_single_game/conftest.py
+++ tests/test_with_single_game/conftest.py
@@ -43,7 +43,9 @@ def create_test_data(vig_app):
     update_scraped_boxscore(vig_app, BBREF_GAME_ID)
     update_scraped_pitch_logs(vig_app, GAME_DATE, BBREF_GAME_ID)
     update_scraped_pitchfx_logs(vig_app, BB_GAME_ID)
-    CombineScrapedDataTask(vig_app).execute(BBREF_GAME_ID, APPPLY_PATCH_LIST)
-    result = AddToDatabaseTask(vig_app).execute(2019)
-    assert result.success
+    combine_data_result_dict = CombineScrapedDataTask(vig_app).execute(BBREF_GAME_ID, APPPLY_PATCH_LIST)
+    assert combine_data_result_dict["gather_scraped_data_success"]
+    assert combine_data_result_dict["combine_data_success"]
+    add_to_db_result = AddToDatabaseTask(vig_app).execute(2019)
+    assert add_to_db_result.success
     return True
