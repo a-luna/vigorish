3e243dec08bfa091ab789f71c29f8c88304230cf
Aaron Luna <contact@aaronluna.dev>
2021-07-27 01:10:16 -0700

    enhancement: track player role in team assoc table

--- src/vigorish/models/player_team_assoc.py
+++ src/vigorish/models/player_team_assoc.py
@@ -14,4 +14,25 @@ class Assoc_Player_Team(db.Base):
     team_id = Column(String)
     year = Column(Integer)
     stint_number = Column(Integer)
+    starting_lineup = Column(Integer, default=0)
+    bench_player = Column(Integer, default=0)
+    starting_pitcher = Column(Integer, default=0)
+    relief_pitcher = Column(Integer, default=0)
+    def_pos_list = Column(String)
+    role_is_defined = Column(Integer, default=0)
+
     season_id = Column(Integer, ForeignKey("season.id"))
+
+    def as_dict(self):
+        return {
+            "mlb_id": self.mlb_id,
+            "bbref_id": self.bbref_id,
+            "team_id": self.team_id,
+            "year": self.year,
+            "stint_number": self.stint_number,
+            "starting_lineup": bool(self.starting_lineup),
+            "bench_player": bool(self.bench_player),
+            "starting_pitcher": bool(self.starting_pitcher),
+            "relief_pitcher": bool(self.relief_pitcher),
+            "def_pos_list": self.def_pos_list,
+        }
