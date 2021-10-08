dde759975a9b3ce9250aafa5c0e211c7c38c5e91
Aaron Luna <contact@aaronluna.dev>
2021-10-07 17:03:12 -0700

    feature: add players who debuted in current season to db backup (#852)

--- src/vigorish/models/__init__.py
+++ src/vigorish/models/__init__.py
@@ -4,7 +4,7 @@ from vigorish.models.batter_percentiles import BatterPercentile
 from vigorish.models.pitch_stats import PitchStats, PitchStatsCsvRow
 from vigorish.models.pitch_type_percentiles import PitchTypePercentile
 from vigorish.models.pitchfx import PitchFx, PitchFxCsvRow
-from vigorish.models.player import Player
+from vigorish.models.player import Player, PlayerCsvRow
 from vigorish.models.player_id import PlayerId
 from vigorish.models.player_team_assoc import Assoc_Player_Team
 from vigorish.models.scrape_error import ScrapeError
